e91ac29ae4aa055a05d7281cea5c737af4d626cd
AlexandreFelippe <alemfelippe@gmail.com>
2024-03-01 09:34:07 -0300

    Requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,21 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("message", "key")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(123, 4)
+
+    assert encrypt_message("message", 0) == "egassem"
+    assert encrypt_message("message", 10) == "egassem"
+
+    assert encrypt_message("message", 3) == "sem_egas"
+
+    assert encrypt_message("message", 4) == "ega_ssem"
+
+    assert encrypt_message("example", 3) == "axe_elpm"
+
+    assert encrypt_message("python", 2) == "noht_yp"
